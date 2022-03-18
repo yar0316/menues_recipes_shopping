616a746e82594b46d05c205010dbf7b7364fa60f
yar0316 <yarecon0316@gmail.com>
2022-03-19 00:37:54 +0900

    roughly added menu list

--- src/components/menues/MenuManagement.vue
+++ src/components/menues/MenuManagement.vue
@@ -21,11 +21,46 @@
                             <v-card-title>{{ getDate(MM_DD_dd, n-1) }}</v-card-title>
                         </v-card-header-text>
                     </v-card-header>
+                    
+                    <v-divider></v-divider>
+                    
                     <v-list>
-                        <v-list-item
-                            v-for="n in 5"
-                        >item{{n}}</v-list-item>
-                    </v-list>
+                        <v-list-group>
+                            <template v-slot:activator="{ props }">
+                                <v-list-item v-bind="props" title="朝食" value="朝食"></v-list-item>
+                            </template>
+
+                            <v-list-item
+                                v-for="breakfast in state.menues[n-1].breakfast"
+                                :title="breakfast.name"
+                                :value="breakfast.id"
+                            ></v-list-item>
+                        </v-list-group>
+
+                        <v-list-group>
+                            <template v-slot:activator="{ props }">
+                                <v-list-item v-bind="props" title="昼食" value="lunch"></v-list-item>
+                            </template>
+
+                            <v-list-item
+                                v-for="lunch in state.menues[n-1].lunch"
+                                :title="lunch.name"
+                                :value="lunch.id"
+                            ></v-list-item>
+                        </v-list-group>
+
+                        <v-list-group>
+                            <template v-slot:activator="{ props }">
+                                <v-list-item v-bind="props" title="夕食" value="dinner"></v-list-item>
+                            </template>
+
+                            <v-list-item
+                                v-for="dinner in state.menues[n-1].dinner"
+                                :title="dinner.name"
+                                :value="dinner.id"
+                            ></v-list-item>
+                        </v-list-group>
+                     </v-list>
                 </v-card>
             </v-col>
         </v-row>
@@ -45,13 +80,240 @@ export default defineComponent({
         const YYYY_MM_DD_dd: string = 'YYYY/MM/DD (dd)'
         const MM_DD_dd: string = 'MM/DD (dd)'
         const referenceDate: dayjs.Dayjs = dayjs().locale('ja')
-        const weekOffset = reactive(ref(1))
+        const state = reactive({
+            menues: [
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                },
+                {
+                    date: '2022/3/29',
+                    breakfast: [
+                        {
+                            id: 0,
+                            name: "目玉焼き",
+                            recipe_id: 0
+                        },
+                        {
+                            id: 1,
+                            name: "トースト"
+                        }
+                    ],
+                    lunch: [
+                        {
+                            id: 2,
+                            name: "カルボナーラ",
+                            recipe_id: 1
+                        }
+                    ],
+                    dinner: [
+                        {
+                            id: 3,
+                            name: "生姜焼き",
+                            recipe_id: 2
+                        },
+                        {
+                            id: 4,
+                            name: "ごはん"
+                        }
+                    ]
+                }
+            ]
+        })
 
         return {
             YYYY_MM_DD_dd,
             MM_DD_dd,
             referenceDate,
-            weekOffset
+            state
         }
 
     },
